7d1d23dd0110247e43b1188ee5116ddafdb24f32
W610-PC03 <labinf@PMAWSW6LC0903.duoc.acad>
2025-09-04 12:22:55 -0400

    Implementación de estructura de archivos

---
+++ js/usuarios.js
@@ -0,0 +1,26 @@
+const usuarios = [
+  {
+    run: "19011022K",
+    nombre: "Juan",
+    apellidos: "Pérez",
+    correo: "admin@duoc.cl",
+    tipo: "Administrador",
+    password: "1234"
+  },
+  {
+    run: "20011033L",
+    nombre: "María",
+    apellidos: "González",
+    correo: "vendedor@gmail.com",
+    tipo: "Vendedor",
+    password: "1234"
+  },
+  {
+    run: "21022044M",
+    nombre: "Carlos",
+    apellidos: "Ramírez",
+    correo: "cliente@gmail.com",
+    tipo: "Cliente",
+    password: "1234"
+  }
+];
